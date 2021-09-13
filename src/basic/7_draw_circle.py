c6b9be7010c508e86532ae5e088bbb1bb6162e33
Caresle <carabesle@gmail.com>
2021-09-13 17:46:53 -0600

    Add draw circle

---
+++ src/basic/7_draw_circle.py
@@ -0,0 +1,27 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Draw circle')
+
+circle_color = (255, 255, 255)
+circle_radius = 32
+circle_center = (WIDTH // 2, HEIGHT // 2)
+
+
+def main():
+	run = True
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		pygame.draw.circle(screen, circle_color, circle_center, circle_radius)
+		pygame.display.update()
+	sys.exit()
+
+
+if __name__ == '__main__':
+	main()
