de8d01a86523849630ef6ad5afd4ecddb40c64e2
Caresle <carabesle@gmail.com>
2021-09-13 18:15:27 -0600

    Add move_rect

---
+++ src/basic/3_move_rect.py
@@ -0,0 +1,40 @@
+# Check the draw/1_draw_rect before this.
+# For understand the Rect for move section
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Move rect')
+
+# Rect for move
+player_rect = pygame.Rect((WIDTH // 2) - 16, (HEIGHT // 2) - 16, 32, 32)
+player_color = (255, 255, 255)
+
+# Speed to move the player
+speed = 1
+
+
+def main():
+	run = True
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		key = pygame.key.get_pressed()
+		if key[pygame.K_LEFT]:
+			player_rect.x -= speed
+		if key[pygame.K_RIGHT]:
+			player_rect.x += speed
+		
+		screen.fill((0, 0, 0))
+		pygame.draw.rect(screen, player_color, player_rect)
+		pygame.display.update()
+	sys.exit()
+
+
+if __name__ == '__main__':
+	main()
