45055ea52130a93672734974567719ae56a123a2
Caresle <carabesle@gmail.com>
2021-09-27 07:32:06 -0600

    Add hsva color example

---
+++ src/color/4_color_hsva.py
@@ -0,0 +1,24 @@
+import pygame, sys
+from pygame.locals import *
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Color hsva')
+
+background_color = pygame.Color(0)
+background_color.hsva = (215, 81, 82, 100)
+
+
+def main():
+	run = True
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		screen.fill(background_color)
+		pygame.display.update()
+	sys.exit()
+
+
+if __name__ == '__main__':
+	main()
