5e8d8a1cb5c5835b0d61aca92236f744685547fb
Caresle <carabesle@gmail.com>
2021-11-24 12:35:00 -0700

    Add load map from file

---
+++ src/image/2_load_gif.py
@@ -0,0 +1,24 @@
+import pygame, sys, os
+from pygame.locals import *
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Load a gif')
+
+img_abspath = os.path.abspath('src/assets/image.gif')
+img_path = os.path.join(os.getcwd(), img_abspath)
+img = pygame.image.load(img_path).convert()
+
+def main():
+	run = True
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		screen.blit(img, (200, 200))
+		pygame.display.update()
+	sys.exit()
+
+
+if __name__ == '__main__':
+	main()
---
+++ src/image/3_draw_from_text.py
@@ -0,0 +1,59 @@
+import pygame, sys
+from pygame.image import load
+from pygame.locals import *
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Draw from text")
+
+# Floor
+floor_img = pygame.image.load("src/assets/floor.png").convert_alpha()
+floor2_img = pygame.image.load("src/assets/floor2.png").convert_alpha()
+floor3_img = pygame.image.load("src/assets/floor3.png").convert_alpha()
+
+
+def load_text(path):
+	f = open(path + '.txt', 'r')
+	data = f.read()
+	f.close()
+	data = data.split('\n')
+	grid_map = []
+	for row in data:
+		grid_map.append(list(row))
+	return grid_map
+
+
+def draw_map(path, screen):
+	grid_map = load_text(path)
+	start_pos = [0, 0]
+	for row in grid_map:
+		for char in row:
+			if char == '0':
+				screen.blit(floor_img, start_pos)
+			if char == '1':
+				screen.blit(floor2_img, start_pos)
+			if char == '2':
+				screen.blit(floor3_img, start_pos)
+			start_pos[0] += 16
+			
+		start_pos[0] = 0
+		start_pos[1] += 16
+
+
+def main():
+	run = True
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+				run = False
+		screen.fill((0, 0, 0))
+		draw_map('src/image/background', screen)
+		pygame.display.update()
+
+
+if __name__ == '__main__':
+	main()
